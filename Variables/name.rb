d80483983d88206edcbd8a82d8d69392cc8a7253
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-09-01 10:24:39 -0500

    9/1

---
+++ Variables/name.rb
@@ -0,0 +1,17 @@
+name=gets.chomp
+puts "Hey there, #{name}!"
+
+
+
+10.times {puts name}
+
+
+
+puts "What is your first name?"
+first=gets.chomp
+puts "What is your last name?"
+last=gets.chomp
+
+
+
+puts "#{first} #{last}"
