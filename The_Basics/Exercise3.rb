90dacdd1380733550b08bd784b5c117f8a9b38ae
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-09-01 08:21:29 -0500

    9/1

---
+++ The_Basics/Exercise3.rb
@@ -0,0 +1,2 @@
+a={Starwars:1982,Clueless:1995,Matrix:2000,AnimalHouse:1980}
+a.each {|a,b| puts b}
---
+++ The_Basics/Exercise4.rb
@@ -0,0 +1,7 @@
+a={Starwars:1982,Clueless:1995,Matrix:2000,AnimalHouse:1980}
+array=[]
+a.each do |a,b|
+  array<<b
+end
+
+print array
